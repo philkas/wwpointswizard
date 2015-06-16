38a05de9bf14854eddb89eb9d609898cc1f20568
Philipp Kastner <philipp.kastner@gmx.de>
2015-06-16 21:47:34 +0200

    implemented points arithmetic

--- app/src/main/java/pika/de/wwpointswizard2/core/wwpoints/PointArithmetic.java
+++ app/src/main/java/pika/de/wwpointswizard2/core/wwpoints/PointArithmetic.java
@@ -4,5 +4,7 @@ package pika.de.wwpointswizard2.core.wwpoints;
  * Created by pika on 16.06.15.
  */
 public interface PointArithmetic {
-
+    double add(Points points);
+    double sub(Points points);
+    Points multiply(double factor);
 }
--- app/src/main/java/pika/de/wwpointswizard2/core/wwpoints/SimplePoints.java
+++ app/src/main/java/pika/de/wwpointswizard2/core/wwpoints/SimplePoints.java
@@ -3,11 +3,11 @@ package pika.de.wwpointswizard2.core.wwpoints;
 /**
  * Created by pika on 14.06.15.
  */
-public class SimplePoints implements Points {
+public class SimplePoints implements Points, PointArithmetic {
     private final double kcal;
     private final double fat;
 
-    public SimplePoints(int kcal, double fat) {
+    public SimplePoints(double kcal, double fat) {
         this.kcal = kcal;
         this.fat = fat;
     }
@@ -23,5 +23,20 @@ public class SimplePoints implements Points {
     @Override
     public double calculatePoints() {
         return getKcal() / 60.0 + getFat() / 9.0;
+    }
+
+    @Override
+    public double add(Points points) {
+        return this.calculatePoints() + points.calculatePoints();
+    }
+
+    @Override
+    public double sub(Points points) {
+        return this.calculatePoints() - points.calculatePoints();
+    }
+
+    @Override
+    public Points multiply(double factor) {
+        return new SimplePoints(this.kcal * factor, this.fat * factor);
     }
 }
