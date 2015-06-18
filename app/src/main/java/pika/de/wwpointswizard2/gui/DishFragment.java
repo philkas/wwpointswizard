576356370bb43873acb1e2a17ee2efb901b8558e
Philipp Kastner <philipp.kastner@gmx.de>
2015-06-18 23:43:43 +0200

    moved fragment

--- app/src/main/java/pika/de/wwpointswizard2/gui/DishFragment.java
+++ app/src/main/java/pika/de/wwpointswizard2/gui/DishFragment.java
@@ -1,4 +1,4 @@
-package pika.de.wwpointswizard2;
+package pika.de.wwpointswizard2.gui;
 
 
 import android.os.Bundle;
@@ -6,6 +6,8 @@ import android.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import pika.de.wwpointswizard2.R;
 
 
 /**
