9740e9c69b8e1fbd22b23b299582a3a7dc96bb98
Philipp Kastner <philipp.kastner@gmx.de>
2015-06-18 23:41:54 +0200

    implemented dishes

--- app/src/main/java/pika/de/wwpointswizard2/core/dishes/Dish.java
+++ app/src/main/java/pika/de/wwpointswizard2/core/dishes/Dish.java
@@ -1,8 +1,100 @@
 package pika.de.wwpointswizard2.core.dishes;
 
+import java.math.BigDecimal;
+
+import pika.de.wwpointswizard2.core.wwpoints.WWPointType;
+
 /**
  * Created by pika on 18.06.15.
  */
 public class Dish {
     private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        if(name == null || name.isEmpty()) {
+            throw new IllegalArgumentException("Argument name must not be null or empty.");
+        }
+        this.name = name;
+    }
+
+    public int getKcal() {
+        return kcal;
+    }
+
+    public void setKcal(int kcal) {
+        this.kcal = kcal;
+    }
+
+    public BigDecimal getFat() {
+        return fat;
+    }
+
+    public void setFat(BigDecimal fat) {
+        this.fat = fat;
+    }
+
+    public BigDecimal getSugar() {
+        return sugar;
+    }
+
+    public void setSugar(BigDecimal sugar) {
+        this.sugar = sugar;
+    }
+
+    public BigDecimal getProtein() {
+        return protein;
+    }
+
+    public void setProtein(BigDecimal protein) {
+        this.protein = protein;
+    }
+
+    public WWPointType getPoints() {
+        return points;
+    }
+
+    public void setPoints(WWPointType points) {
+        if(points == null) {
+            throw new IllegalArgumentException("Argument points must not be null");
+        }
+        this.points = points;
+    }
+
+    public String getBarcode() {
+        return barcode;
+    }
+
+    public void setBarcode(String barcode) {
+        this.barcode = barcode;
+    }
+
+    public Portion getPortion() {
+        return portion;
+    }
+
+    public void setPortion(Portion portion) {
+        if(portion == null) {
+            throw new IllegalArgumentException("Argument portion must not be null");
+        }
+        this.portion = portion;
+    }
+
+    private int kcal;
+    private BigDecimal fat;
+    private BigDecimal sugar;
+    private BigDecimal protein;
+    private WWPointType points;
+    private String barcode;
+    private Portion portion;
+
+    public Dish(String name, WWPointType points, Portion portion) {
+        setName(name);
+        setPoints(points);
+        setPortion(portion);
+    }
+
 }
