37d105c6d4fd553ea07bb2f5b782a3e52dfe20a3
Philipp Kastner <philipp.kastner@gmx.de>
2015-06-19 11:07:59 +0200

    dish service javadoc and impl

--- app/src/main/java/pika/de/wwpointswizard2/services/DishService.java
+++ app/src/main/java/pika/de/wwpointswizard2/services/DishService.java
@@ -6,5 +6,9 @@ import java.util.List;
  * Created by pika on 19.06.15.
  */
 public interface DishService {
+    /**
+     * Get a list of units for the portion
+     * @return A list of units using the shortname.
+     */
     List<String> getPortionShortnames();
 }
--- app/src/main/java/pika/de/wwpointswizard2/services/impl/DishServiceImpl.java
+++ app/src/main/java/pika/de/wwpointswizard2/services/impl/DishServiceImpl.java
@@ -1,7 +1,25 @@
 package pika.de.wwpointswizard2.services.impl;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import pika.de.wwpointswizard2.core.dishes.PortionUnit;
+import pika.de.wwpointswizard2.services.DishService;
+
 /**
  * Created by pika on 19.06.15.
  */
-public class DishServiceImpl {
+public class DishServiceImpl implements DishService {
+    /**
+     * Collects all units defined in {@link PortionUnit} and returns a list.
+     * @return A list of all units from {@link PortionUnit}
+     */
+    @Override
+    public List<String> getPortionShortnames() {
+        List<String> units = new ArrayList<>(PortionUnit.values().length);
+        for(PortionUnit unit : PortionUnit.values()) {
+            units.add(unit.getShortname());
+        }
+        return units;
+    }
 }
