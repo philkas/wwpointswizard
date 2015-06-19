4130a5326a7b4bacd85d8a1721cad7e929009c03
Philipp Kastner <philipp.kastner@gmx.de>
2015-06-19 10:52:23 +0200

    show portion units in spinner

--- app/src/main/java/pika/de/wwpointswizard2/gui/DishFragment.java
+++ app/src/main/java/pika/de/wwpointswizard2/gui/DishFragment.java
@@ -6,14 +6,19 @@ import android.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import pika.de.wwpointswizard2.R;
+import pika.de.wwpointswizard2.services.DishService;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class DishFragment extends Fragment {
+public class DishFragment extends Fragment implements AdapterView.OnItemSelectedListener {
+    private DishService dishService;
 
 
     public DishFragment() {
@@ -25,8 +30,33 @@ public class DishFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_dish, container, false);
+        View view = inflater.inflate(R.layout.fragment_dish, container, false);
+        Spinner portionUnitSpinner = getPortionUnitSpinner();
+        portionUnitSpinner.setOnItemSelectedListener(this);
+        return view;
+    }
+
+    private Spinner getPortionUnitSpinner() {
+        return (Spinner) getActivity().findViewById(R.id.spinPortionUnit);
+    }
+
+    private ArrayAdapter<String> getSpinnerItemAdapter() {
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(
+                this.getActivity(),
+                android.R.layout.simple_spinner_item,
+                dishService.getPortionShortnames());
+        adapter.setDropDownViewResource(android.R.layout.simple_dropdown_item_1line);
+        return adapter;
     }
 
 
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
+    }
 }
--- app/src/main/java/pika/de/wwpointswizard2/services/DishService.java
+++ app/src/main/java/pika/de/wwpointswizard2/services/DishService.java
@@ -1,7 +1,10 @@
 package pika.de.wwpointswizard2.services;
 
+import java.util.List;
+
 /**
  * Created by pika on 19.06.15.
  */
 public interface DishService {
+    List<String> getPortionShortnames();
 }
